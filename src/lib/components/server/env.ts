1a7c286705dcb963791d46c87c2a42be120c8ce3
Martin <mselvenis@gmail.com>
2023-07-16 15:48:30 +0100

    server side ENV configuration

---
+++ src/lib/components/server/env.ts
@@ -0,0 +1,17 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+function getEnvironmentVariable(envirnomentVariable: string): string {
+    const validEnvironmentVariable = process.env[envirnomentVariable];
+    if (!validEnvironmentVariable) {
+        throw new Error(`Could not find environment variable: ${envirnomentVariable}`)
+    }
+    return validEnvironmentVariable;
+}
+
+export const ENV = {
+    PUBLIC_SUPABASE_ANON_KEY: getEnvironmentVariable("PUBLIC_SUPABASE_ANON_KEY"),
+    PUBLIC_SUPABASE_API_URL: getEnvironmentVariable("PUBLIC_SUPABASE_API_URL"),
+    SUPABASE_SERVICE_ROLE_KEY: getEnvironmentVariable("SUPABASE_SERVICE_ROLE_KEY"),
+    SUPABASE_DB_URL: getEnvironmentVariable("SUPABASE_DB_URL")
+}
