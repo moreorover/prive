f07bcaaef4751594c6d5e9fb85d310281c697055
Martin <mselvenis@gmail.com>
2023-07-17 19:27:59 +0100

    server side supabase configuration

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,19 @@
+import type { Database } from "$lib/supabase-types";
+import type { Session, SupabaseClient } from "@supabase/supabase-js";
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
+		interface Locals {
+			supabase: SupabaseClient<Database>,
+			getSession(): Promise<Session | null>
+		}
+		interface PageData {
+			session: Session | null
+		}
 		// interface Platform {}
 	}
 }
 
-export {};
+export { };
+
