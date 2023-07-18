301634081c0ec12a55dc886edaf74a9d02876f61
Martin <mselvenis@gmail.com>
2023-07-18 20:36:27 +0100

    3.6 account actions

--- src/routes/account/+page.server.ts
+++ src/routes/account/+page.server.ts
@@ -1,7 +1,7 @@
 import { emailSchema, passwordSchema, profileSchema } from "$lib/schemas";
-import { error, redirect } from "@sveltejs/kit";
-import { superValidate } from "sveltekit-superforms/server";
-import type { PageServerLoad } from "./$types";
+import { error, fail, redirect } from "@sveltejs/kit";
+import { setError, superValidate } from "sveltekit-superforms/server";
+import type { Actions, PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async (event) => {
 	const session = await event.locals.getSession();
@@ -30,4 +30,91 @@ export const load: PageServerLoad = async (event) => {
 		emailForm: superValidate({ email: session.user.email }, emailSchema, { id: "email" }),
 		passwordForm: superValidate(passwordSchema, { id: "password" })
 	};
+};
+
+export const actions: Actions = {
+	updateProfile: async (event) => {
+		const session = await event.locals.getSession();
+		if (!session) {
+			throw error(401, "Unauthorized");
+		}
+
+		const profileForm = await superValidate(event, profileSchema, { id: "profile" });
+
+		if (!profileForm.valid) {
+			return fail(400, {
+				profileForm
+			});
+		}
+
+		const { error: profileError } = await event.locals.supabase
+			.from("profiles")
+			.update(profileForm.data)
+			.eq("id", session.user.id);
+
+		if (profileError) {
+			return setError(profileForm, null, "Error updateing your profile.");
+		}
+
+		return {
+			profileForm
+		};
+	},
+	updateEmail: async (event) => {
+		const session = await event.locals.getSession();
+		if (!session) {
+			throw error(401, "Unauthorized");
+		}
+
+		const emailForm = await superValidate(event, emailSchema, { id: "email" });
+
+		if (!emailForm.valid) {
+			return fail(400, {
+				emailForm
+			});
+		}
+
+		const { error: emailError } = await event.locals.supabase.auth.updateUser({
+			email: emailForm.data.email
+		});
+
+		if (emailError) {
+			return setError(emailForm, "email", "Error updateing your profile.");
+		}
+
+		return {
+			emailForm
+		};
+	},
+	updatePassword: async (event) => {
+		const session = await event.locals.getSession();
+		if (!session) {
+			throw error(401, "Unauthorized");
+		}
+
+		const passwordForm = await superValidate(event, passwordSchema, {
+			id: "password"
+		});
+
+		if (!passwordForm.valid) {
+			return fail(400, {
+				passwordForm
+			});
+		}
+
+		if (passwordForm.data.password !== passwordForm.data.passwordConfirm) {
+			return setError(passwordForm, "passwordConfirm", "Passwords must match");
+		}
+
+		const { error: passwordError } = await event.locals.supabase.auth.updateUser({
+			password: passwordForm.data.password
+		});
+
+		if (passwordError) {
+			return setError(passwordForm, null, "Error updating your password");
+		}
+		return {
+			passwordForm
+		};
+	}
 };
