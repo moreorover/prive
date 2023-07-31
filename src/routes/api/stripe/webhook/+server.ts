9f86c9d728365a8f03163f5c01e1edae3533706d
Martin <mselvenis@gmail.com>
2023-07-31 20:17:09 +0100

    7 Billing Services

--- src/routes/api/stripe/webhook/+server.ts
+++ src/routes/api/stripe/webhook/+server.ts
@@ -1,13 +1,14 @@
+import { deleteCustomerRecord, updateCustomerRecord } from "$lib/server/customers";
 import { ENV } from "$lib/server/env";
+import { deleteProductRecord, upsertProductRecord } from "$lib/server/products";
 import { stripe } from "$lib/server/stripe";
+import { insertSubscriptionRecord, updateSubscriptionRecord } from "$lib/server/subscriptions";
 import { json } from "@sveltejs/kit";
 import type Stripe from "stripe";
 import type { RequestHandler } from "./$types";
 
 export const POST: RequestHandler = async (event) => {
 	const stripeSignature = event.request.headers.get("stripe-signature");
-
-	console.log({ stripeSignature });
 
 	if (!stripeSignature) {
 		console.log("stripe -> Unauthorized");
@@ -29,36 +30,30 @@ export const POST: RequestHandler = async (event) => {
 			ENV.STRIPE_SIGNING_SECRET
 		) as Stripe.DiscriminatedEvent;
 	} catch (e) {
-		console.log("stripe -> Invalid Signature");
 		return json("Invalid signature", { status: 401 });
 	}
 
 	try {
-		console.log(`stripe event -> ${stripeEvent.type}`);
 		switch (stripeEvent.type) {
 			case "product.created":
-				console.log("Product created", stripeEvent);
-				break;
 			case "product.updated":
-				console.log("Product updated", stripeEvent);
+				await upsertProductRecord(stripeEvent.data.object);
 				break;
 			case "product.deleted":
-				console.log("Product deleted", stripeEvent);
+				await deleteProductRecord(stripeEvent.data.object);
 				break;
 			case "customer.updated":
-				console.log("customer updated", stripeEvent);
+				await updateCustomerRecord(stripeEvent.data.object);
 				break;
 			case "customer.deleted":
-				console.log("Customer deleted", stripeEvent);
+				await deleteCustomerRecord(stripeEvent.data.object);
 				break;
 			case "customer.subscription.created":
-				console.log("Customer Subscription created", stripeEvent);
+				await insertSubscriptionRecord(stripeEvent.data.object);
 				break;
 			case "customer.subscription.updated":
-				console.log("Customer Subscription updated", stripeEvent);
-				break;
 			case "customer.subscription.deleted":
-				console.log("Customer Subscription deleted", stripeEvent);
+				await updateSubscriptionRecord(stripeEvent.data.object);
 				break;
 			case "customer.subscription.trial_will_end":
 				console.log("Customer Subscription trial will end", stripeEvent);
