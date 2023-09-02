bcd3719cfea22ce130efc5fe6073765cdcbd2434
Martin <mselvenis@gmail.com>
2023-09-02 23:55:41 +0100

    page template

---
+++ src/lib/components/ProfileDrawer.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { getDrawerStore } from "@skeletonlabs/skeleton";
+
+	const drawerStore = getDrawerStore();
+
+	function drawerClose(): void {
+		drawerStore.close();
+	}
+</script>
+
+<nav class="list-nav p-4">
+	<ul>
+		<li><a href="/" on:click={drawerClose}>Notes</a></li>
+		<li><a href="/?test=1" on:click={drawerClose}>Other Page 1</a></li>
+		<li><a href="/?test=2" on:click={drawerClose}>Other Page 2</a></li>
+	</ul>
+	<hr />
+	<ul>
+		<li><a href="/logout" on:click={drawerClose}>Log Out</a></li>
+	</ul>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,44 +1,85 @@
 <script lang="ts">
-	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import Navigation from "$lib/components/Navigation.svelte";
+	import ProfileDrawer from "$lib/components/ProfileDrawer.svelte";
+	import {
+		AppBar,
+		AppShell,
+		Avatar,
+		Drawer,
+		Modal,
+		Toast,
+		getDrawerStore,
+		initializeStores
+	} from "@skeletonlabs/skeleton";
+	import "../app.postcss";
+
+	initializeStores();
+
+	const drawerStore = getDrawerStore();
+
+	function navigationDrawerOpen(): void {
+		drawerStore.open({
+			id: "navigation",
+			position: "left"
+		});
+	}
+
+	function profileDrawerOpen(): void {
+		drawerStore.open({
+			id: "profile",
+			position: "right"
+		});
+	}
 </script>
 
+<Drawer>
+	<h2 class="p-4">Prive</h2>
+	<hr />
+	{#if $drawerStore.id === "navigation"}
+		<Navigation />
+	{:else if $drawerStore.id === "profile"}
+		<ProfileDrawer />
+	{/if}
+</Drawer>
+
+<Toast position="br" />
+
+<Modal />
+
 <!-- App Shell -->
-<AppShell>
+<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<div class="flex items-center">
+					<button class="lg:hidden btn btn-sm mr-4" on:click={navigationDrawerOpen}>
+						<span>
+							<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+								<rect width="100" height="20" />
+								<rect y="30" width="100" height="20" />
+								<rect y="60" width="100" height="20" />
+							</svg>
+						</span>
+					</button>
+					<a href="/">
+						<strong class="text-xl uppercase">Prive</strong>
+					</a>
+				</div>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+				<a class="btn btn-sm variant-ghost-primary" href="/login"> Login </a>
+				<a class="btn btn-sm variant-ghost-secondary" href="/register"> Register </a>
+				<Avatar
+					border="border-4 border-surface-300-600-token hover:!border-primary-500"
+					cursor="cursor-pointer"
+					on:click={profileDrawerOpen} />
+				<a class="btn btn-sm variant-ghost-tertiary" href="/logout">Log Out</a>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
-	<!-- Page Route Content -->
+	<svelte:fragment slot="sidebarLeft">
+		<Navigation />
+	</svelte:fragment>
 	<slot />
 </AppShell>
