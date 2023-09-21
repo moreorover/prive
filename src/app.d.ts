daa6baad21aa117c78962ff88007b4bd99e166b6
Martin <mselvenis@gmail.com>
2023-09-21 22:17:42 +0100

    fix types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,17 +1,19 @@
 import type { Database } from "$lib/supabase-types";
 import type { Session, SupabaseClient } from "@supabase/supabase-js";
 
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-// and what to do when importing types
-declare namespace App {
-	// interface Error {}
-	interface Locals {
-		supabase: SupabaseClient<Database>;
-		getSession(): Promise<Session | null>;
+declare global {
+	/// <reference types="stripe-event-types" />
+	namespace App {
+		// interface Error {}
+		interface Locals {
+			supabase: SupabaseClient<Database>;
+			getSession(): Promise<Session | null>;
+		}
+		interface PageData {
+			session: Session | null;
+		}
+		// interface Platform {}
 	}
-	interface PageData {
-		session: Session | null;
-	}
-	// interface Platform {}
 }
+
+export {};
