658edb6d5710e95cec38c7f2b086362d7c80d144
Martin <mselvenis@gmail.com>
2023-09-24 19:51:43 +0100

    added light switch

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
-	import { AppBar, AppShell, Avatar, getDrawerStore } from "@skeletonlabs/skeleton";
+	import {
+		AppBar,
+		AppShell,
+		autoModeWatcher,
+		Avatar,
+		getDrawerStore,
+		LightSwitch
+	} from "@skeletonlabs/skeleton";
 
 	const drawerStore = getDrawerStore();
 
@@ -11,6 +18,9 @@
 	}
 </script>
 
+<svelte:head
+	>{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}</svelte:head>
+
 <AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
@@ -21,12 +31,13 @@
 				</a>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a class="btn btn-sm variant-ghost-primary" href="/login"> Login </a>
-				<a class="btn btn-sm variant-ghost-secondary" href="/register"> Register </a>
+				<a class="variant-ghost-primary btn btn-sm" href="/login"> Login </a>
+				<a class="variant-ghost-secondary btn btn-sm" href="/register"> Register </a>
 				<Avatar
 					border="border-4 border-surface-300-600-token hover:!border-primary-500"
 					cursor="cursor-pointer"
 					on:click={profileDrawerOpen} />
+				<LightSwitch />
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
