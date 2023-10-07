701f746239a8b4a39108d214149eca0a4e980005
Martin <mselvenis@gmail.com>
2023-10-07 23:58:42 +0100

    ability to manage roles

--- src/routes/admin/users/[userId]/+page.svelte
+++ src/routes/admin/users/[userId]/+page.svelte
@@ -4,4 +4,14 @@
 	export let data: PageData;
 </script>
 
-<h1>{data.userId}</h1>
+<div class="card p-4">
+	<h1>{data.user.full_name}</h1>
+</div>
+
+<div class="card mt-4 p-4">
+	{#each data.user.user_roles as role}
+		<h1>{role.role}</h1>
+	{/each}
+	<a href="/admin/users/{data.user.id}/updateRoles" class="variant-filled-surface btn btn-sm"
+		>Update Roles</a>
+</div>
