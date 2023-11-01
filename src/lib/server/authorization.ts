c3986ed337d6bc456908573ff3b331ba3dddb5a9
Martin <mselvenis@gmail.com>
2023-11-01 19:54:01 +0000

    clear up

--- src/lib/server/authorization.ts
+++
@@ -1,5 +0,0 @@
-export type UserRole = "Admin" | "Moderator";
-
-export const userHasRole = (userRoles: UserRole[], role: UserRole): boolean => {
-	return userRoles.includes(role);
-};
--- src/routes/admin/+layout.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-</script>
-
-<div class="container mx-auto mt-4">
-	<slot />
-</div>
