9bc4a985bb2138e3610e97ebe39daecf5c6deea4
Martin <mselvenis@gmail.com>
2023-12-02 22:27:43 +0000

    using SideBarNavigation.svelte component within /admin portal

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,9 +1,66 @@
 <script lang="ts">
 	import Navigation from '$lib/components/Navigation.svelte';
 	import type { PageData } from './$types';
+	import SideBarNavigation from '$lib/components/SideBarNavigation.svelte';
 	export let data: PageData;
+
+	const sidebarNavItems = [
+		{
+			title: 'Admin',
+			href: '/admin'
+		},
+		{
+			title: 'Stock',
+			href: '/admin/stock'
+		},
+		{
+			title: 'Stock',
+			href: '/admin/stock'
+		},
+		{
+			title: 'Stock',
+			href: '/admin/stock'
+		},
+		{
+			title: 'Stock',
+			href: '/admin/stock'
+		},
+		{
+			title: 'Stock',
+			href: '/admin/stock'
+		},
+		{
+			title: 'Stock',
+			href: '/admin/stock'
+		},
+		{
+			title: 'Stock',
+			href: '/admin/stock'
+		},
+		{
+			title: 'Stock',
+			href: '/admin/stock'
+		},
+		{
+			title: 'Stock',
+			href: '/admin/stock'
+		},
+		{
+			title: 'Stock',
+			href: '/admin/stock'
+		}
+	];
 </script>
 
 <Navigation class="mx-6" authenticated={true} user={data.session.user} roles={data.roles} />
 
-<slot />
+<div class="space-y-6 p-10 pb-16 md:block">
+	<div class="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+		<aside class="-mx-4 lg:w-1/6">
+			<SideBarNavigation items={sidebarNavItems} />
+		</aside>
+		<div class="flex-1 lg:max-w-2xl">
+			<slot />
+		</div>
+	</div>
+</div>
