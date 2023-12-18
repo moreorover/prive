3c21a98304d623fbfbe5617e1738a9dd9b8207e5
Martin <mselvenis@gmail.com>
2023-12-18 22:25:23 +0000

    Update admin clients layout with role check and creation feature
    In this update, TypeScript code and UI components are added to the admin's client page. Now, it checks if the user has the 'Admin' role and, if so, it enables a button to create a new client. An interactive h2 tag is also introduced for user-friendly navigation.

--- src/routes/admin/clients/+page.svelte
+++ src/routes/admin/clients/+page.svelte
@@ -1,1 +1,22 @@
-<h1>Clients</h1>
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { Button } from '$lib/components/ui/button';
+	import { Plus } from 'lucide-svelte';
+
+	export let data: PageData;
+</script>
+
+<div class="flex-1 space-y-4 p-8 pt-6">
+	<div class="flex items-center justify-between space-y-2">
+		<h2 class="text-3xl font-bold tracking-tight">Clients</h2>
+		<div class="flex items-center space-x-2">
+			{#if data.roles.includes('Admin')}
+				<Button size="sm" href="/admin/clients/new">
+					<Plus class="mr-2 h-4 w-4" />
+					Create
+				</Button>
+			{/if}
+		</div>
+	</div>
+	<div>Form</div>
+</div>
