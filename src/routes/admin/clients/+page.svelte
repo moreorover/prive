cedd00b74fa8442d1d29cf9a422b76827cb53812
Martin <mselvenis@gmail.com>
2023-12-29 10:19:42 +0000

    cleaner and simpler look to the side bar navigation. Dead 'Command' and 'Popover' code segments were removed as they were no longer necessary.

--- src/routes/admin/clients/+page.svelte
+++ src/routes/admin/clients/+page.svelte
@@ -5,8 +5,6 @@
 	import * as Avatar from '$lib/components/ui/avatar';
 	import { Button } from '$lib/components/ui/button';
 	import * as Card from '$lib/components/ui/card';
-	import * as Command from '$lib/components/ui/command';
-	import * as Popover from '$lib/components/ui/popover';
 
 	export let data: PageData;
 
@@ -54,43 +52,8 @@
 						{/if}
 					</div>
 				</div>
-				<!--				<Popover.Root>-->
-				<!--					<Popover.Trigger asChild let:builder>-->
-				<!--						<Button builders={[builder]} variant="outline" class="ml-auto">Owner</Button>-->
-				<!--					</Popover.Trigger>-->
-				<!--					<Popover.Content class="p-0" align="end">-->
-				<!--						<Command.Root>-->
-				<!--							<Command.Input placeholder="Select new role..." />-->
-				<!--							<Command.List>-->
-				<!--								<Command.Empty>No roles found.</Command.Empty>-->
-				<!--								<Command.Group>-->
-				<!--									<Command.Item class="space-y-1 flex flex-col items-start px-4 py-2">-->
-				<!--										<p>Viewer</p>-->
-				<!--										<p class="text-sm text-muted-foreground">Can view and comment.</p>-->
-				<!--									</Command.Item>-->
-				<!--									<Command.Item class="space-y-1 flex flex-col items-start px-4 py-2">-->
-				<!--										<p>Developer</p>-->
-				<!--										<p class="text-sm text-muted-foreground">Can view, comment, and edit.</p>-->
-				<!--									</Command.Item>-->
-				<!--									<Command.Item class="space-y-1 flex flex-col items-start px-4 py-2">-->
-				<!--										<p>Billing</p>-->
-				<!--										<p class="text-sm text-muted-foreground">-->
-				<!--											Can view, comment and manage billing.-->
-				<!--										</p>-->
-				<!--									</Command.Item>-->
-				<!--									<Command.Item class="space-y-1 flex flex-col items-start px-4 py-2">-->
-				<!--										<p>Owner</p>-->
-				<!--										<p class="text-sm text-muted-foreground">-->
-				<!--											Admin-level access to all resources.-->
-				<!--										</p>-->
-				<!--									</Command.Item>-->
-				<!--								</Command.Group>-->
-				<!--							</Command.List>-->
-				<!--						</Command.Root>-->
-				<!--					</Popover.Content>-->
-				<!--				</Popover.Root>-->
+				<Button variant="secondary" href={`/admin/clients/${client.id}`}>View</Button>
 			</div>
 		{/each}
 	</Card.Content>
 </Card.Root>
-<!--</div>-->
