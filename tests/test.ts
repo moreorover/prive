688ed60423891a0d55f32213f38fb95ec09ea1b1
Martin <mselvenis@gmail.com>
2023-12-29 11:42:54 +0000

    Update test for index page's navigation bar heading
    Changed the test to check for the visibility of 'Prive' in the navigation bar

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('index page has expected navigation bar heading text', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Prive' })).toBeVisible();
 });
