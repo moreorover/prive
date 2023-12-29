f314a2138bdbd9319099dac6b417f78efdf58a6a
Martin <mselvenis@gmail.com>
2023-12-29 11:13:44 +0000

    Add ClientUpdateFormDialog component and backend support
    Added a new ClientUpdateFormDialog.svelte component for client information update and integrated it in the +page.svelte file. Also, implemented server side action 'updateClient' using the existing clientSchema in the '+page.server.ts' file. The new

--- src/routes/admin/clients/[clientId]/+page.server.ts
+++ src/routes/admin/clients/[clientId]/+page.server.ts
@@ -2,6 +2,7 @@ import { error, fail, redirect } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
 import { setError, superValidate } from 'sveltekit-superforms/server';
 import { handleLoginRedirect } from '$lib/helpers';
+import { clientSchema } from '$lib/schema/clientSchema';
 
 export const load: PageServerLoad = async (event) => {
 	const session = await event.locals.getSession();
@@ -26,37 +27,37 @@ export const load: PageServerLoad = async (event) => {
 		return contact;
 	}
 	return {
-		client: await getClient(event.params.clientId)
-		// updateContactForm: superValidate(await getClient(event.params.clientId), createContactSchema)
+		client: await getClient(event.params.clientId),
+		updateClientForm: superValidate(await getClient(event.params.clientId), clientSchema)
 	};
 };
 
-// export const actions: Actions = {
-// 	updateContact: async (event) => {
-// 		const session = await event.locals.getSession();
-// 		if (!session) {
-// 			throw error(401, 'Unauthorized');
-// 		}
-//
-// 		const updateContactForm = await superValidate(event, createContactSchema);
-//
-// 		if (!updateContactForm.valid) {
-// 			return fail(400, {
-// 				updateContactForm
-// 			});
-// 		}
-//
-// 		const { error: updateContactError } = await event.locals.supabase
-// 			.from('contacts')
-// 			.update(updateContactForm.data)
-// 			.eq('id', event.params.contactId);
-//
-// 		if (updateContactError) {
-// 			return setError(updateContactForm, null, 'Error updating contact, please try again later.');
-// 		}
-//
-// 		return {
-// 			updateContactForm
-// 		};
-// 	}
-// };
+export const actions: Actions = {
+	updateClient: async (event) => {
+		const session = await event.locals.getSession();
+		if (!session) {
+			throw error(401, 'Unauthorized');
+		}
+
+		const updateContactForm = await superValidate(event, clientSchema);
+
+		if (!updateContactForm.valid) {
+			return fail(400, {
+				updateContactForm
+			});
+		}
+
+		const { error: updateContactError } = await event.locals.supabase
+			.from('clients')
+			.update(updateContactForm.data)
+			.eq('id', event.params.clientId);
+
+		if (updateContactError) {
+			return setError(updateContactForm, 'Error updating contact, please try again later.');
+		}
+
+		return {
+			updateContactForm
+		};
+	}
+};
--- src/routes/admin/clients/[clientId]/+page.svelte
+++ src/routes/admin/clients/[clientId]/+page.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import * as Card from '$lib/components/ui/card';
+	import ClientUpdateFormDialog from '$lib/components/ClientUpdateFormDialog.svelte';
 
 	export let data: PageData;
 </script>
 
 <Card.Root>
 	<Card.Header>
-		<Card.Title>{data.client.name}</Card.Title>
+		<div class="flex items-center justify-between space-y-2">
+			<div>
+				<Card.Title>{data.client.name}</Card.Title>
+			</div>
+			<div>
+				<ClientUpdateFormDialog data={data.updateClientForm} />
+			</div>
+		</div>
 	</Card.Header>
 	<Card.Content class="grid gap-6" />
 </Card.Root>
